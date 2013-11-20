986b14b1883aec45bd08b262e85e8ceacbebb8d9
Benjamin Eikel <benjamin@eikel.org>
2013-11-20 14:03:00 +0100

    Add an example for using GUI
    It uses GUI's API from C++ code to create a Window, a Textfield, and a Button.

---
+++ examples/TextfieldAndButton/TextfieldAndButtonMain.cpp
@@ -0,0 +1,100 @@
+/*
+	This file is part of the GUI library.
+	Copyright (C) 2013 Benjamin Eikel <benjamin@eikel.org>
+	
+	This library is subject to the terms of the Mozilla Public License, v. 2.0.
+	You should have received a copy of the MPL along with this library; see the 
+	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+#include <GUI/Components/Button.h>
+#include <GUI/Components/Textfield.h>
+#include <GUI/Components/Window.h>
+#include <GUI/GUI_Manager.h>
+#include <Util/UI/EventContext.h>
+#include <Util/UI/UI.h>
+#include <Util/References.h>
+#include <Util/StringIdentifier.h>
+#include <Util/Util.h>
+#include <cstdlib>
+#include <iostream>
+
+/**
+ * @file
+ * @brief Simple usage example for GUI
+ * 
+ * The example uses a GUI::GUI_Manager to create a GUI::Window, a
+ * GUI::Textfield, and a GUI::Button. Then, it registers a GUI::ActionListener
+ * to wait for a click onto the button. When the button is clicked, the text
+ * field is cleared.
+ */
+
+int main(int /*argc*/, char */*argv*/[]) {
+	Util::init();
+
+	Util::UI::Window::Properties properties;
+	properties.positioned = true;
+	properties.posX = 100;
+	properties.posY = 100;
+	properties.clientAreaWidth = 1024;
+	properties.clientAreaHeight = 768;
+	properties.title = "GUI Textfield and Buttons";
+	properties.compatibilityProfile = true;
+	auto window = Util::UI::createWindow(properties);
+
+	Util::UI::EventContext eventContext;
+	eventContext.getEventQueue().registerEventGenerator(std::bind(&Util::UI::Window::fetchEvents, window.get()));
+	
+	Util::Reference<GUI::GUI_Manager> guiManager = new GUI::GUI_Manager(eventContext);
+	guiManager->setWindow(window.get());
+
+	Util::Reference<GUI::Window> guiWin = guiManager->createWindow(Geometry::Rect_f(10, 10, 200, 200), "Window");
+
+	Util::Reference<GUI::Textfield> guiText = guiManager->createTextfield(Geometry::Rect_f(0, 0, 40, 20), "Text");
+	guiWin->addContent(guiText.get());
+
+	Util::StringIdentifier clearActionId("clear_text");
+	Util::Reference<GUI::Button> guiButton = guiManager->createButton(Geometry::Rect_f(0, 25, 40, 20), "Clear", clearActionId);
+	guiWin->addContent(guiButton.get());
+	
+	struct ClearTextAction : public GUI::ActionListener {
+		const Util::StringIdentifier listenAction;
+		Util::Reference<GUI::Textfield> textField;
+
+		ClearTextAction(Util::StringIdentifier whenToClear,
+						Util::Reference<GUI::Textfield> whatToClear) :
+			listenAction(std::move(whenToClear)),
+			textField(std::move(whatToClear)) {
+		}
+
+		virtual ~ClearTextAction() = default;
+
+		GUI::listenerResult_t handleAction(GUI::Component * /*component*/, const Util::StringIdentifier & actionName) override {
+			if(actionName == listenAction) {
+				textField->setText("");
+				return GUI::LISTENER_EVENT_CONSUMED;
+			}
+			return GUI::LISTENER_EVENT_NOT_CONSUMED;
+		}
+	};
+	ClearTextAction action(clearActionId, guiText);
+	guiManager->registerActionListener(&action);
+
+	bool done = false;
+	while(!done) {
+		eventContext.getEventQueue().process();
+		while(eventContext.getEventQueue().getNumEventsAvailable() > 0) {
+			auto event = eventContext.getEventQueue().popEvent();
+			if(event.type == Util::UI::EVENT_QUIT ||
+						(event.type == Util::UI::EVENT_KEYBOARD &&
+						 event.keyboard.pressed &&
+						 event.keyboard.key == Util::UI::KEY_ESCAPE)) {
+				done = true;
+			} else {
+				guiManager->handleEvent(event);
+			}
+		}
+		guiManager->display();
+		window->swapBuffers();
+	}
+	return EXIT_SUCCESS;
+}
