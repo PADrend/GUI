PADrend/GUI

commit 309ee8b905db2a5af2183c3a779b841fe082b308

Change return type of onMouseButton to bool
Only LISTENER_EVENT_NOT_CONSUMED and LISTENER_EVENT_CONSUMED were
returned before. Therefore, I replaced it by false and true.
Furthermore, the logic removing the listener after the call was removed.

Components/ScrollableContainer.cpp
@@ -110,18 +110,18 @@ void ScrollableContainer::doLayout() {
 }
 
 //! ---|> MouseButtonListener
-listenerResult_t ScrollableContainer::onMouseButton(Component * /*component*/, const Util::UI::ButtonEvent & buttonEvent){
+bool ScrollableContainer::onMouseButton(Component * /*component*/, const Util::UI::ButtonEvent & buttonEvent){
 	if(buttonEvent.button == Util::UI::MOUSE_BUTTON_MIDDLE) {
 		if(maxScrollPos.x()<=0 && maxScrollPos.y()<=0)
-			return LISTENER_EVENT_NOT_CONSUMED;
+			return false;
 		else if(buttonEvent.pressed){
 			getGUI().addMouseMotionListener(this);
 		} else {// !pressed
 			getGUI().removeMouseMotionListener(this);
 		}
-		return LISTENER_EVENT_CONSUMED;
+		return true;
 	}
-	return LISTENER_EVENT_NOT_CONSUMED;
+	return true;
 
 }
 

Base/Listener.h
@@ -122,7 +122,7 @@ struct MouseMotionListener {
 };
 struct MouseButtonListener {
 	static RegisteredListenerRegistry<MouseButtonListener> & getListenerRegistry();
-	virtual listenerResult_t onMouseButton(Component * component, const Util::UI::ButtonEvent & buttonEvent) = 0;
+	virtual bool onMouseButton(Component * component, const Util::UI::ButtonEvent & buttonEvent) = 0;
 	virtual ~MouseButtonListener() {}
 };
 

Components/Button.cpp
@@ -112,9 +112,9 @@ void Button::doDisplay(const Geometry::Rect & region){
 }
 
 //! ---|> MouseButtonListener
-listenerResult_t Button::onMouseButton(Component * /*component*/, const Util::UI::ButtonEvent & buttonEvent){
+bool Button::onMouseButton(Component * /*component*/, const Util::UI::ButtonEvent & buttonEvent){
 	if(buttonEvent.button == Util::UI::MOUSE_BUTTON_MIDDLE) {
-		return LISTENER_EVENT_NOT_CONSUMED;
+		return false;
 	}
 	if(buttonEvent.pressed && !isLocked()){
 		select();
@@ -122,7 +122,7 @@ listenerResult_t Button::onMouseButton(Component * /*component*/, const Util::UI
 			activate();
 		}
 	}
-	return LISTENER_EVENT_CONSUMED;
+	return true;
 }
 
 bool Button::onKeyEvent(const Util::UI::KeyboardEvent & keyEvent) {

Components/Button.h
@@ -51,7 +51,7 @@ class Button : public Container, public MouseMotionListener, public MouseButtonL
 		// ---|> MouseMotionListener
 		virtual listenerResult_t onMouseMove(Component * component, const Util::UI::MotionEvent & motionEvent) override;
 		// ---|> MouseButtonListener
-		virtual listenerResult_t onMouseButton(Component * component, const Util::UI::ButtonEvent & buttonEvent) override;
+		virtual bool onMouseButton(Component * component, const Util::UI::ButtonEvent & buttonEvent) override;
 
 	private:
 		// ---|> Component

Components/Checkbox.cpp
@@ -93,14 +93,14 @@ void Checkbox::doDisplay(const Geometry::Rect & region){
 }
 
 //! ---|> MouseButtonListener
-listenerResult_t Checkbox::onMouseButton(Component * /*component*/, const Util::UI::ButtonEvent & buttonEvent){
+bool Checkbox::onMouseButton(Component * /*component*/, const Util::UI::ButtonEvent & buttonEvent){
 	if(buttonEvent.pressed){
 		select();
 		if(buttonEvent.button == Util::UI::MOUSE_BUTTON_LEFT) {
 			activate();
 		}
 	}
-	return LISTENER_EVENT_CONSUMED;
+	return true;
 
 }
 

Components/Checkbox.h
@@ -46,7 +46,7 @@ class Checkbox : public Container, public MouseButtonListener {
 		virtual void action();
 
 		// ---|> MouseButtonListener
-		virtual listenerResult_t onMouseButton(Component * component, const Util::UI::ButtonEvent & buttonEvent) override;
+		virtual bool onMouseButton(Component * component, const Util::UI::ButtonEvent & buttonEvent) override;
 
 		// ---|> Component
 		virtual void doLayout() override;

Components/EditorPanel.cpp
@@ -125,7 +125,7 @@ listenerResult_t EditorPanel::onMouseMove(Component * /*component*/, const Util:
 static const Util::StringIdentifier dataId_marking("marking");
 
 //! ---|> MouseButtonListener
-listenerResult_t EditorPanel::onMouseButton(Component * /*component*/, const Util::UI::ButtonEvent & buttonEvent){
+bool EditorPanel::onMouseButton(Component * /*component*/, const Util::UI::ButtonEvent & buttonEvent){
 	if(buttonEvent.pressed && getGUI().isCtrlPressed()) {
 		if(buttonEvent.button == Util::UI::MOUSE_WHEEL_UP) {
 			float scale=0.8;
@@ -134,7 +134,7 @@ listenerResult_t EditorPanel::onMouseButton(Component * /*component*/, const Uti
 				child->setWidth(child->getWidth()*scale);
 				child->setHeight(child->getHeight()*scale);
 			}
-			return LISTENER_EVENT_CONSUMED;
+			return true;
 		} else if(buttonEvent.button == Util::UI::MOUSE_WHEEL_DOWN) {
 
 			float scale=1.0/0.8;
@@ -143,7 +143,7 @@ listenerResult_t EditorPanel::onMouseButton(Component * /*component*/, const Uti
 				child->setWidth(child->getWidth()*scale);
 				child->setHeight(child->getHeight()*scale);
 			}
-			return LISTENER_EVENT_CONSUMED;
+			return true;
 		}
 	}
 	const Geometry::Vec2 localPos = Geometry::Vec2(buttonEvent.x, buttonEvent.y) - getAbsPosition();
@@ -154,7 +154,7 @@ listenerResult_t EditorPanel::onMouseButton(Component * /*component*/, const Uti
 				for(const auto & markedChild : getMarkedChildren()) {
 					if(markedChild->coversLocalPos(localPos - markedChild->getPosition())) {
 						move_start(localPos);
-						return LISTENER_EVENT_CONSUMED;
+						return true;
 					}
 				}
 				for(Component * child=getFirstChild();child!=nullptr;child=child->getNext()){
@@ -165,28 +165,28 @@ listenerResult_t EditorPanel::onMouseButton(Component * /*component*/, const Uti
 					markChild(child);
 					getGUI().componentDataChanged(this,dataId_marking);
 					move_start(localPos);
-					return LISTENER_EVENT_CONSUMED;
+					return true;
 				}
 				rectSelect_start(localPos);
 //				getGUI().componentMouseClicked(this,button,pos,pressed); // ???
-				return LISTENER_EVENT_CONSUMED;
+				return true;
 
 			}else if(buttonEvent.button == Util::UI::MOUSE_BUTTON_RIGHT) {
 				if(!getMarkedChildren().empty()){
 					unmarkAll();
 					getGUI().componentDataChanged(this,dataId_marking);
 				}
-				return LISTENER_EVENT_CONSUMED;
+				return true;
 			}
 			break;
 		}
 		case DRAG_SELECTING:{
 			if(!buttonEvent.pressed && buttonEvent.button == Util::UI::MOUSE_BUTTON_LEFT) {
 				rectSelect_finish(localPos);
-				return LISTENER_EVENT_CONSUMED;
+				return true;
 			} else if(buttonEvent.pressed && buttonEvent.button == Util::UI::MOUSE_BUTTON_RIGHT) {
 				rectSelect_break();
-				return LISTENER_EVENT_CONSUMED;
+				return true;
 			}
 
 			break;
@@ -194,17 +194,17 @@ listenerResult_t EditorPanel::onMouseButton(Component * /*component*/, const Uti
 		case MOVING:{
 			if(!buttonEvent.pressed && buttonEvent.button == Util::UI::MOUSE_BUTTON_LEFT) {
 				move_finish();
-				return LISTENER_EVENT_CONSUMED;
+				return true;
 			} else if(buttonEvent.pressed && buttonEvent.button == Util::UI::MOUSE_BUTTON_RIGHT) {
 				move_break(localPos);
-				return LISTENER_EVENT_CONSUMED;
+				return true;
 			}
 			break;
 		}
 		default:
 			WARN("unexpected case in switch statement");
 	}
-	return LISTENER_EVENT_NOT_CONSUMED;
+	return false;
 }
 
 void EditorPanel::rectSelect_start(const Vec2 & pos){

Components/EditorPanel.h
@@ -35,7 +35,7 @@ class EditorPanel: public Container,public MouseButtonListener,public MouseMotio
 		// ---|> MouseMotionListener
 		virtual listenerResult_t onMouseMove(Component * component, const Util::UI::MotionEvent & motionEvent) override;
 		// ---|> MouseButtonListener
-		virtual listenerResult_t onMouseButton(Component * component, const Util::UI::ButtonEvent & buttonEvent) override;
+		virtual bool onMouseButton(Component * component, const Util::UI::ButtonEvent & buttonEvent) override;
 
 		// ---|> Container
 		virtual void removeContent(const Ref & child) override	{

commit 477314631fbbd820dda35e0b1b4c204f8da2093b

Correct return value when event is not consumed
The wrong value was introduced by commit
309ee8b905db2a5af2183c3a779b841fe082b308.
This change fixes #8.

Components/ScrollableContainer.cpp
@@ -117,7 +117,7 @@ bool ScrollableContainer::onMouseButton(Component * /*component*/, const Util::U
 		}
 		return true;
 	}
-	return true;
+	return false;
 
 }
 